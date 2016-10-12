9ada088c9875a4d8a7a78841f3ecf91f8f71f2c3
Kirk Byers <ktbyers@twb-tech.com>
2016-10-12 10:48:29 -0700

    NAPALM exercise2

--- day2/napalm_ex2.py
+++ day2/napalm_ex2.py
@@ -2,10 +2,10 @@
 from pprint import pprint as pp
 
 from napalm_base import get_network_driver
-from my_devices import device_list
+from my_devices import pynet_rtr1
 
 def main():
-    for a_device in device_list:
+    for a_device in (pynet_rtr1,):
         device_type = a_device.pop('device_type')
         driver = get_network_driver(device_type)
         device = driver(**a_device)
@@ -14,9 +14,21 @@ def main():
         print ">>>Device open"
         device.open()
 
-        print "-" * 50
-        device_facts = device.get_facts()
-        print "{hostname}: Model={model}".format(**device_facts)
+        print
+        print ">>>Load config change (merge) - no commit"
+        device.load_merge_candidate(filename='cisco_merge.txt')
+        print device.compare_config()
+
+        print
+        print ">>>Discard config change (merge)"
+        device.discard_config()
+        print device.compare_config()
+
+        print
+        print ">>>Load config change (merge) - commit"
+        device.load_merge_candidate(filename='cisco_merge.txt')
+        print device.compare_config()
+        device.commit_config()
 
     print
 
